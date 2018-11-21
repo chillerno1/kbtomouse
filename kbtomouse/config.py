4c2f8a4be91f0e58e55518e921655731d82b75d9
chillerno1 <kurtis.frost@iinet.net.au>
2018-11-21 12:09:03 +1100

    Update config.py

--- kbtomouse/config.py
+++ kbtomouse/config.py
@@ -3,6 +3,6 @@ import pyautogui
 class Config(object):
     SCREEN_HEIGHT = pyautogui.size()[0]
     SCREEN_WIDTH = pyautogui.size()[1]
-    TRAVEL_DIST = 0.01 
+    TRAVEL_DIST = 0.01 # % of screen to move on each keypress
     MODIFIER = 'ctrl'
     SHOW_LOG = True
